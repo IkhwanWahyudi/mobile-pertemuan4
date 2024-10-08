5f730e9bf957313656081c9eca12b9d23477bb5c
IkhwanWahyudi <ikhwanw06@gmail.com>
2024-10-08 09:14:32 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan5/pages/account_page.dart';
+import 'package:pertemuan5/pages/home_page.dart';
+import 'package:pertemuan5/pages/settings_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pertemuan 5',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/home': (context) => const HomePage(),
+        '/setting': (context) => const SettingsPage(),
+        '/account': (context) => const AccountPage(),
+      },
+      home: const HomePage(),
+    );
+  }
+}
